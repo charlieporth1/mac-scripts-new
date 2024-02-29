b22c295755492f088b6e2467e9e3c93ee65745fd
Charles Porth <charlieporth1@gmail.com>
2024-02-29 13:28:15 -0600

    Fixed script  mk-cloud-storage-links.sh

--- mk-cloud-storage-links.sh
+++ mk-cloud-storage-links.sh
@@ -5,13 +5,15 @@ lib_dir=$usr_dir/Library
 cloud_sg_dir=$lib_dir/CloudStorage
 
 syno_dir=$cloud_sg_dir/SynologyDrive-Sync
-cloud_dirs=( $( ls -d $cloud_sg_dir/*/ )
+cloud_dirs=( $( ls -d $cloud_sg_dir/*/ ) )
 
 for dir in ${cloud_dirs[@]}
 do
+	cut_dir=$( echo $dir | awk -F- '{print $1}' )
+
 	ln -s $dir $usr_dir/
+	ln -s $cut_dir $usr_dir/
+
 done
 
-if [[ -d $syno_dir ]]; then
-	ln -s $syno_dir $usr_dir
-fi
+
