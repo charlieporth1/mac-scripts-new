61e29a76525add6db16801991d0297159a672015
Charles Porth <charlieporth1@gmail.com>
2024-02-29 13:13:57 -0600

    fixed set-fanspeed.sh if-then

--- set-fanspeed.sh
+++ set-fanspeed.sh
@@ -33,7 +33,7 @@ do
 		;;
 		--download-smc )
 			# https://brew.sh/
-			if ! command -v brew;
+			if ! command -v brew; then
 				/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 			fi
 
